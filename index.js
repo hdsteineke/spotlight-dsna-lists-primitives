de67f6a388c7fd9c69452cce34425e24239cfb13
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-11-20 12:28:37 -0800

    drafting oddishOrEvenish

--- index.js
+++ index.js
@@ -32,4 +32,24 @@ function titleCase(sentence) {
     .map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
 }
 
-module.exports = { reverseWords, titleCase };
+//////////////////////////////////////
+
+function oddishOrEvenish(number) {
+  const initialValue = 0;
+  // edge case for empty parameters
+  if (!number) return "Invalid entry";
+  //need to split number into an array of individual digits (needs to be string first)
+  const numArray = number.toString().split("");
+  //sum individual digits together
+  const sum = numArray.reduce(
+    (acc, currentValue) => acc + currentValue, initialValue
+    );
+
+  //if sum is even, return evenish
+  if (sum % 2 === 0) return "Evenish";
+  return "Oddish";
+
+  //else return oddish
+}
+
+module.exports = { reverseWords, titleCase, oddishOrEvenish };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { reverseWords, titleCase } = require("./index.js");
+const { reverseWords, titleCase, oddishOrEvenish } = require("./index.js");
 
 describe("reverseWords", () => {
   it("should reverse the words of a sentence", () => {
@@ -9,5 +9,18 @@ describe("reverseWords", () => {
 describe("titleCase", () => {
   it("should return a sentence with the first letter of each word capitalized)", () => {
     expect(titleCase("happy birthday to you")).toBe("Happy Birthday To You");
+  });
+});
+
+describe("oddishOrEvenish", () => {
+  it("should determine if sum of input digits is odd or even", () => {
+    expect(oddishOrEvenish(121)).toBe("Evenish");
+  });
+});
+
+
+describe("oddishOrEvenish", () => {
+  it("should determine if sum of input digits is odd or even", () => {
+    expect(oddishOrEvenish(191)).toBe("Oddish");
   });
 });
